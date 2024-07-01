7504b2f71e7366cda70049b56e455ae4a48b4bb0
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-07-01 11:27:38 -0600

    Commit verificación

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname + "/public")));
 
-app.get('/verificationCustomer', (req, res) => {
+app.post('/verificationCustomer', (req, res) => {
 
     console.log("REQUEST: " + JSON.stringify(req.body));
 
