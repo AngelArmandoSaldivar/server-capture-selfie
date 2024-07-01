a21cacb39851a4cbee666debc0af8b0fb7b3e0e7
AngelArmandoSaldivar <ang.arm.saldivar@gmail.com>
2024-07-01 13:57:17 -0600

    Commit verificación

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { json, urlencoded } = require("body-parser");
+const path = require('path');
 const app = express()
 /*app.use(express.json());
 app.use(express.urlencoded({extended: true}));*/
